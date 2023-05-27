9877f3346069b90011d5d4d696930375795e4279
LuisMirandaU202113647 <134754386+LuisMirandaU202113647@users.noreply.github.com>
2023-05-26 21:40:03 -0500

    Add files via upload

---
+++ TrabajoFinalCRUD/src/main/java/com/upc/TrabajoFinalCRUD/TrabajoFinalCrudApplication.java
@@ -0,0 +1,13 @@
+package com.upc.TrabajoFinalCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoFinalCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoFinalCrudApplication.class, args);
+	}
+
+}
