c3bd3c1498d6c631f553e2cf115055cc440e8382
Mauro solorzano <94402510+czpm@users.noreply.github.com>
2023-05-28 15:40:23 -0500

    Add files via upload

---
+++ TrabajoFinalCRUD/src/test/java/com/upc/TrabajoFinalCRUD/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.upc.TrabajoFinalCRUD.controller;
+
+import com.upc.TrabajoFinalCRUD.model.Item;
+import com.upc.TrabajoFinalCRUD.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+    @GetMapping ("/name/{id}")
+    private ResponseEntity<Item> getItem (@PathVariable (value = "id")Long itemId){
+        return new ResponseEntity<Item>(itemService.getItem(itemId), HttpStatus.OK);
+    }
+    @PostMapping("/insert")
+    private ResponseEntity<Item> insertItem (@RequestBody Item itemDetalle){
+        return new ResponseEntity<Item>(itemService.insertItem(itemDetalle), HttpStatus.CREATED);
+    }
+    @DeleteMapping ("/delete/{id}")
+    private ResponseEntity<Item> deleteItem (@PathVariable (value = "id")Long itemId){
+        return new ResponseEntity<Item>(itemService.delteItem(itemId), HttpStatus.OK);
+    }
+}
