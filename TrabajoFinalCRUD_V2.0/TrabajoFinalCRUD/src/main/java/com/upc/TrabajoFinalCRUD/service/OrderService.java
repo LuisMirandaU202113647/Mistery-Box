3d67e924cb0f90800bad8111e0cbb8cfa3d212ec
marcelochagray17 <134242363+marcelochagray17@users.noreply.github.com>
2023-06-20 17:32:39 -0500

    Add files via upload

--- TrabajoFinalCRUD_V2.0/TrabajoFinalCRUD/src/main/java/com/upc/TrabajoFinalCRUD/service/OrderService.java
+++ TrabajoFinalCRUD_V2.0/TrabajoFinalCRUD/src/main/java/com/upc/TrabajoFinalCRUD/service/OrderService.java
@@ -1,10 +1,7 @@
 package com.upc.TrabajoFinalCRUD.service;
 
-import com.upc.TrabajoFinalCRUD.model.dto.OrderDTO;
-import com.upc.TrabajoFinalCRUD.model.entity.Customer;
 import com.upc.TrabajoFinalCRUD.model.entity.MysteryBox;
 import com.upc.TrabajoFinalCRUD.model.entity.Order;
-import com.upc.TrabajoFinalCRUD.repository.CustomerRepository;
 import com.upc.TrabajoFinalCRUD.repository.ItemRepository;
 import com.upc.TrabajoFinalCRUD.repository.MysteryBoxRepository;
 import com.upc.TrabajoFinalCRUD.repository.OrderRepository;
@@ -20,10 +17,6 @@ import java.util.List;
 public class OrderService {
     @Autowired
     OrderRepository orderRepository;
-    @Autowired
-    MysteryBoxRepository mysteryBoxRepository;
-    @Autowired
-    CustomerRepository customerRepository;
 
     @Transactional (readOnly = true)
     public List<Order> getAllOrders(){
@@ -34,20 +27,5 @@ public class OrderService {
         return orderRepository.findById(orderId)
                 .orElseThrow(()->new OpenApiResourceNotFoundException("No se encuentra la orden con Id "+orderId));
     }
-    @Transactional
-    public Order insertOrder(OrderDTO orderDetails){
-        Order order=new Order();
-        MysteryBox mysteryBox;
-        Customer customer;
-        order.setId(orderDetails.getId());
-        mysteryBox=mysteryBoxRepository.findById(orderDetails.getMysteryBoxId())
-                .orElseThrow(()-> new OpenApiResourceNotFoundException("No se encuentra la mystery box con el Id "+orderDetails.getMysteryBoxId()));
-        customer=customerRepository.findById(orderDetails.getCustomerId())
-                .orElseThrow(()->new OpenApiResourceNotFoundException("No se encuentra el cliente con el Id "+orderDetails.getCustomerId()));
-        order.setCustomer(customer);
-        order.setMysteryBox(mysteryBox);
-        order.setDate(orderDetails.getDate());
-        order.setTotalPrice(mysteryBox.getPrice());
-        return orderRepository.save(order);
-    }
+
 }
