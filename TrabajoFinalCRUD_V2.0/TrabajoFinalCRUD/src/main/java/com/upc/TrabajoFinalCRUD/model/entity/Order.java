37a94e360b1ed20e33b45ff954a52ca245cc4084
marcelochagray17 <134242363+marcelochagray17@users.noreply.github.com>
2023-06-20 17:34:47 -0500

    Add files via upload

--- TrabajoFinalCRUD_V2.0/TrabajoFinalCRUD/src/main/java/com/upc/TrabajoFinalCRUD/model/entity/Order.java
+++ TrabajoFinalCRUD_V2.0/TrabajoFinalCRUD/src/main/java/com/upc/TrabajoFinalCRUD/model/entity/Order.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="order_mb")
 public class Order {
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
@@ -20,7 +21,6 @@ public class Order {
     @ManyToOne()
     @JoinColumn(name="customerId")
     private Customer customer;
-    private String date;
+    private Date date;
     private Long totalPrice;
-
 }
