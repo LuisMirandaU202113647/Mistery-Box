7e44c1abda5b16846ed3c55fbe042d53d066a05e
Luis Alfredo Miranda Valera <134754386+LuisMirandaU202113647@users.noreply.github.com>
2024-07-18 16:15:00 -0500

    Delete TrabajoFinalCRUD_V3.0 directory

--- TrabajoFinalCRUD_V3.0/src/main/java/com/upc/TrabajoFinalCRUD/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.TrabajoFinalCRUD.repository;
-
-import com.upc.TrabajoFinalCRUD.model.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
