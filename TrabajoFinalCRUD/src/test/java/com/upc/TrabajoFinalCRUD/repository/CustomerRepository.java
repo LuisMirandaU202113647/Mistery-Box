a67e43e506e4bd2636db2823b44827078f9b62ff
LuisMirandaU202113647 <134754386+LuisMirandaU202113647@users.noreply.github.com>
2024-07-18 15:46:30 -0500

    Delete TrabajoFinalCRUD directory

--- TrabajoFinalCRUD/src/test/java/com/upc/TrabajoFinalCRUD/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.upc.TrabajoFinalCRUD.repository;
-
-import com.upc.TrabajoFinalCRUD.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
